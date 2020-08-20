8bfa66114119c93e1d2e922cfed335f53d14de17
Sujit Singh <sujitkumarsingh29@gmail.com>
2020-08-20 09:19:46 +0530

    Initial Commit with State management setup

---
+++ src/screen/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Main = () => {
+  return (
+    <View>
+      <Text>Main App</Text>
+    </View>
+  );
+};
+
+export default Main;
