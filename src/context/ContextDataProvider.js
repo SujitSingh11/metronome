c452e052c8e8ce1b0fdc20083cd14c9b6e8ec077
Sujit Singh <sujitkumarsingh29@gmail.com>
2020-08-24 23:26:34 +0530

    Update ContextDataProvider.js

--- src/context/ContextDataProvider.js
+++ src/context/ContextDataProvider.js
@@ -11,22 +11,21 @@ export const ContextDataProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
+    const readItemFromStorage = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem("@APP_STATE");
+
+        return dispatch({
+          type: "FETCH_STORAGE",
+          payload: jsonValue != null ? JSON.parse(jsonValue) : null,
+        });
+      } catch (e) {
+        // error reading value
+        console.warn(e);
+      }
+    };
     readItemFromStorage();
   }, []);
-
-  const readItemFromStorage = async () => {
-    try {
-      const jsonValue = await AsyncStorage.getItem("@APP_STATE");
-
-      return dispatch({
-        type: "FETCH_STORAGE",
-        payload: jsonValue != null ? JSON.parse(jsonValue) : null,
-      });
-    } catch (e) {
-      // error reading value
-      console.warn(e);
-    }
-  };
 
   // Actions: Here we will write our actions and use dispatch function which we got from the useReducer Hook, which will then call the reducer with apporiate action type and payload
 
